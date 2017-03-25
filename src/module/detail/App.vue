5ef7e76e629fae59cf520e9180e2a22a61fc3edb
chenlei0804 <c709660962@163.com>
2017-03-25 18:08:51 +0800

    vue+webpack

---
+++ src/module/detail/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./images/logo.png">
+    <h1>这是详情页面</h1>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import Hello from 'components/Hello/Hello'
+
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/module/home/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./images/logo.png">
+    
+    <hello></hello>
+
+    <h1>这是首页</h1>
+    <a href="./detail.html">第二个页面</a>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import Hello from 'components/Hello/Hello'
+
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
